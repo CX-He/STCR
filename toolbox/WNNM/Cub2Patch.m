10e09d7835bbed99ba754a8161c653ccdda3b392
CX-He <111898807+CX-He@users.noreply.github.com>
2023-03-17 15:49:34 +0800

    Add files via upload

--- toolbox/WNNM/Cub2Patch.m
+++ toolbox/WNNM/Cub2Patch.m
@@ -1,7 +1,6 @@
 function  [Y, Mat]  =  Cub2Patch( E_Img, Average, par )
 TotalPatNum = (size(E_Img,1)-par.patsize+1)*(size(E_Img,2)-par.patsize+1);                  %Total Patch Number in the image
 Y           =   zeros(par.patsize*par.patsize*size(E_Img,3), TotalPatNum, 'single');        %Current Patches
-% N_Y         =   zeros(par.patsize*par.patsize*size(E_Img,3), TotalPatNum, 'single');        %Patches in the original noisy image
 Mat         =   zeros(par.patsize*par.patsize, TotalPatNum, 'single');                      %Patches in the original noisy image
 k           =   0;
 for o = 1:size(E_Img,3)
@@ -9,13 +8,10 @@ for o = 1:size(E_Img,3)
         for j  = 1:par.patsize
             k     =  k+1;
             E_patch     =  E_Img(i:end-par.patsize+i,j:end-par.patsize+j,o);
-%             N_patch     =  N_Img(i:end-par.patsize+i,j:end-par.patsize+j,o);
-            Y(k,:)      =  E_patch(:)';
-%             N_Y(k,:)    =  N_patch(:)';            
+            Y(k,:)      =  E_patch(:)';       
         end
     end
 end
-
 k           =   0;
 for i  = 1:par.patsize
     for j  = 1:par.patsize
